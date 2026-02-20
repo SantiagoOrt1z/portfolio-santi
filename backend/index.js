16dd0d5d53e81b92858e739e662cc33d3e077b96
SantiagoOrt1z <santiagoelmeister0609gmail.com>
2026-02-19 22:22:04 -0300

    Cambio para Versel

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,13 @@ import terminalRoutes from "./routes/terminal.route.js";
 
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
 
 app.use("/api", terminalRoutes);
 
-app.listen(3001, () => {
-  console.log("Backend running on http://localhost:3001");
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
